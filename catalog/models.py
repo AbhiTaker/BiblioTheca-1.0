a68f1bbcc48e0ba74287a4004891669000c6dea2
AbhiTaker <Abhi.taker20@gmail.com>
2017-05-27 09:37:35 +0530

    data added

--- catalog/models.py
+++ catalog/models.py
@@ -26,8 +26,7 @@ class Book(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        
-        return reverse('book-detail', args=[str(self.id)])
+          return reverse('book-detail', args=[str(self.id)])
 
 class BookInstance(models.Model):
     
--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-
+    url(r'^$', views.index, name='index'),
 ]
+
+s
